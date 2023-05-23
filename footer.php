1fce86c81d752f533ece19a71256c0898685b41e
Pelle Nilsen <pellemnilsen@gmail.com>
2023-05-23 20:59:30 +0200

    Setup
    Created the basic html setup

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <!-- Insert footer information here -->
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+    $page_title = "Home - My Drone Photography";
+    include('header.php');
+?>
+
+<!-- Hero Section -->
+<div id="hero">
+    <img src="images\DJI_0010.JPG" alt="Fredrikstad Sentrum">
+    <h1>Welcome to My Drone Photography</h1>
+    <p>Capturing the world from a different perspective</p>
+    <a href="#about">Learn More</a>
+</div>
+
+<!-- About Section -->
+<div id="about">
+    <h2>About Me</h2>
+    <p>Brief introduction about me and me service.</p>
+    <a href="about.php">Read More</a>
+</div>
+
+<!-- Portfolio Section -->
+<div id="portfolio">
+    <h2>My Portfolio</h2>
+    <p>A selection of your best work</p>
+    <a href="portfolio.php">View more</a>
+</div>
+
+<!-- Testimonials Section -->
+<div id="testimonials">
+    <h2>Testimonials</h2>
+    <!-- Insert testimonials here -->
+</div>
+
+<!-- Contact CTA -->
+<div id="contact-cta">
+    <h2>Let's Work Togheter</h2>
+    <p>If you like my work and want to work togheter, get in touch!</p>
+    <a href="contact.php">Contact Me</a>
+</div>
+
+<?php include('footer.php'); ?>
